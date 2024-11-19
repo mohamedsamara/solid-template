b1cb6903df3ff54530f64b8911f7821bc48e9f91
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2024-11-19 11:15:34 -0800

    build: add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import type { Component } from "solid-js";
 
 const App: Component = () => {
-  return <h1>Hello</h1>;
+  return (
+    <div class="min-h-screen flex items-center justify-center bg-blue-100">
+      <h1 class="text-4xl font-bold text-blue-600">
+        Hello, Solid.js with Tailwind!
+      </h1>
+    </div>
+  );
 };
 
 export default App;
